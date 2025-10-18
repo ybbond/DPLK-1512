ea7744fe54de0f011a98a239e37c31620833c6d0
Yohanes Bandung Bondowoso <hi@ybbond.id>
2025-10-18 16:36:18 +0700

    refactor minesweeper to be more optimized, and format indentation

--- exe7-minesweeper.cpp
+++ exe7-minesweeper.cpp
@@ -3,92 +3,68 @@
 using namespace std;
 
 int main() {
-  int rowSize, columnSize;
-  int count = 1;
+    int rowSize, columnSize;
+    int count = 1;
 
-  while (true) {
-      cin >> rowSize;
-      cin >> columnSize;
+    const int neighborRow[]    = { -1, -1, -1,  0,  1,  1,  1,  0 };
+    const int neighborColumn[] = { -1,  0,  1,  1,  1,  0, -1, -1 };
 
-      if (rowSize <= 0 || columnSize <= 0) break;
+    while (cin >> rowSize >> columnSize && (rowSize > 0 && columnSize > 0)) {
+        // {
+        //   { '*', '.', '.', '.' },
+        //   { '.', '.', '.', '.' },
+        //   { '.', '*', '.', '.' },
+        //   { '.', '.', '.', '.' }
+        // }
+        // [0][0] → [0][1], [1][0], [1][1]
+        // [2][1] → [1][0], [1][1], [1][2], [2][0], [2][2], [3][0], [3][1], [3][2]
+        char fields[rowSize][columnSize];
+        int result[rowSize][columnSize];
 
-      // {
-      //   { '*', '.', '.', '.' },
-      //   { '.', '.', '.', '.' },
-      //   { '.', '*', '.', '.' },
-      //   { '.', '.', '.', '.' }
-      // }
-      // [0][0] → [0][1], [1][0], [1][1]
-      // [2][1] → [1][0], [1][1], [1][2], [2][0], [2][2], [3][0], [3][1], [3][2]
-      char fields[rowSize][columnSize];
-      int result[rowSize][columnSize];
-
-      for (int r = 0; r < rowSize; r++) {
-        for (int c = 0; c < columnSize; c++) {
-          cin >> fields[r][c];
-          result[r][c] = 0;
+        for (int r = 0; r < rowSize; r++) {
+            for (int c = 0; c < columnSize; c++) {
+                cin >> fields[r][c];
+                result[r][c] = 0;
+            }
         }
-      }
 
-      for (int r = 0; r < rowSize; r++) {
-        for (int c = 0; c < columnSize; c++) {
-          if (fields[r][c] == '.') continue; // early return
+        for (int r = 0; r < rowSize; r++) {
+            for (int c = 0; c < columnSize; c++) {
+                if (fields[r][c] == '.') continue; // early return
 
-          // kiri atas
-          if (r - 1 >= 0 && c - 1 >= 0 && fields[r - 1][c - 1] != '*') {
-            result[r - 1][c - 1]++;
-          }
+                for (int i = 0; i < 8; i++) {
+                    int nr = r + neighborRow[i];
+                    int nc = c + neighborColumn[i];
 
-          // atas
-          if (r - 1 >= 0 && fields[r - 1][c] != '*') {
-            result[r - 1][c]++;
-          }
-
-          // kanan atas
-          if (r - 1 >= 0 && c + 1 < columnSize && fields[r - 1][c + 1] != '*') {
-            result[r - 1][c + 1]++;
-          }
-
-          // kiri
-          if (c - 1 >= 0 && fields[r][c - 1] != '*') {
-            result[r][c - 1]++;
-          }
-
-          // kanan
-          if (c + 1 < columnSize && fields[r][c + 1] != '*') {
-            result[r][c + 1]++;
-          }
-
-          // kiri bawah
-          if (r + 1 < rowSize && c - 1 >= 0 && fields[r + 1][c - 1] != '*') {
-            result[r + 1][c - 1]++;
-          }
-
-          // bawah
-          if (r + 1 < rowSize && fields[r + 1][c] != '*') {
-            result[r + 1][c]++;
-          }
-
-          // kanan bawah
-          if (r + 1 < rowSize && c + 1 < columnSize && fields[r + 1][c + 1] != '*') {
-            result[r + 1][c + 1]++;
-          }
+                    if (
+                      nr >= 0 &&
+                      nr < rowSize &&
+                      nc >= 0 &&
+                      nc < columnSize &&
+                      fields[nr][nc] != '*'
+                    ) {
+                        result[nr][nc]++;
+                    }
+                }
+            }
         }
-      }
 
-      cout << "Field #" << count << ":" << endl;
+        cout << endl << "Field #" << count << ":" << endl;
 
-      for (int r = 0; r < rowSize; r++) {
-        for (int c = 0; c < columnSize; c++) {
-          if (fields[r][c] == '*') {
-            cout << '*';
-          } else {
-            cout << result[r][c];
-          }
+        for (int r = 0; r < rowSize; r++) {
+            for (int c = 0; c < columnSize; c++) {
+                if (fields[r][c] == '*') {
+                    cout << '*';
+                } else {
+                    cout << result[r][c];
+                }
+            }
+            cout << endl;
         }
+
         cout << endl;
-      }
-  }
+        count++;
+    }
 
-  return 0;
+    return 0;
 }
